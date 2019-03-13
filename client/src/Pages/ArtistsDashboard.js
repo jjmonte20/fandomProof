328f6db885af05510856be5e042419bf6d7131c3
jjmonte20 <jjmonte20@gmail.com>
2019-03-13 05:52:00 -0500

    added search with artist fanchain used by description of blockchain

--- client/src/Pages/ArtistsDashboard.js
+++ client/src/Pages/ArtistsDashboard.js
@@ -9,9 +9,12 @@ class Artist extends Component {
         name: "",
         numFollowers: "",
         popularity: "",
-        images: []
+        images: [],
+        tokens: [],
+        search: ""
     }
     componentDidMount() {
+        this.loadTokens();
         API.getArtistInfo(this.state.id).then(res => {
             let images = this.state.images;
             for (var i in res.data.images) {
@@ -29,17 +32,61 @@ class Artist extends Component {
         });
     }
 
+    loadTokens = () => {
+        API.loadPublisherTokens().then(res => {
+            this.setState({ tokens: res.data });
+            console.log(this.state.tokens);
+        });
+    }
+
+    handleInputChange = e => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
     render() {
+        let filteredArtists = this.state.tokens.filter(
+            (artist) => {
+                return artist.description.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            }
+        )
         return (
             <div className="container">
                 <b>{this.state.name}</b><br/>
-                You're an artist <br/><br/>
+                Universal's Dashboard <br/><br/>
                 <b>Spotify Information</b><br/>
                 You have {this.state.numFollowers} followers on Spotify.<br/>
                 Your popularity on Spotify is {this.state.popularity}.
 
                 <br/><br/> Images: {JSON.stringify(this.state.images)}
+                <br/><br/>
+                <form>
+                    <input
+                        className="form-control"
+                        type="search"
+                        name="search"
+                        value={this.state.search}
+                        placeholder="Search"
+                        aria-label="Search"
+                        onChange={this.handleInputChange}
+                        />
+                </form>
+                <div>
+                    Artist FanChain: {filteredArtists.length}
+                    {filteredArtists.length ? (
+                        <div className="card">
+                            {filteredArtists.map(artist => (
+                                <div className="card-body">
+                                <strong>{artist.description}</strong>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <h3>Cannot find any Artists</h3>
+                    )}
+                </div>
             </div>
         );
     }
